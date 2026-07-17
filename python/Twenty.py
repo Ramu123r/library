4e332294ee696f80fe0cf29da3833f7f5c73496c
Ramu123r <137395046+Ramu123r@users.noreply.github.com>
2026-07-17 19:34:06 +0530

    Delete python/Twenty.py

--- python/Twenty.py
+++
@@ -1,8 +0,0 @@
-# 20. Write a python program to demonstrate exception handling
-try:
-    a = 10
-    b = 0
-    c = a / b
-    print(c)
-except ZeroDivisionError:
-    print("Cannot divide by zero")
